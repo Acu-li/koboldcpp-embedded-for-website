424085273f04ff7a16a32d79b2688627108b4300
Moritz Nickel <166907943+Fischherboot@users.noreply.github.com>
2024-08-22 22:26:02 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+APP = ['koboldcpp.py']
+DATA_FILES = [
+    'koboldcpp_default.so',
+    'ggml-metal-merged.metal',
+    'kcpp_adapters',
+    'klite.embd',
+    'kcpp_docs.embd',
+    'kcpp_sdui.embd',
+    'taesd.embd',
+    'taesd_xl.embd',
+    'rwkv_vocab.embd',
+    'rwkv_world_vocab.embd'
+]
+OPTIONS = {
+    'argv_emulation': True,
+    'includes': ['customtkinter', 'psutil'],
+    'packages': [],
+    'plist': {
+        'CFBundleName': 'Koboldcpp',
+        'CFBundleDisplayName': 'Koboldcpp',
+        'CFBundleIdentifier': 'com.yourdomain.koboldcpp',
+        'CFBundleVersion': '0.1.0',
+        'CFBundleShortVersionString': '0.1.0',
+    }
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
